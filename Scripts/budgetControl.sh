ba71a8028ee2fd7428a7513625e7737372db3752
Joel Fernandes <joel.junior88@gmail.com>
2024-06-04 11:24:37 -0300

    Budget e Permissoes

--- Scripts/budgetControl.sh
+++ Scripts/budgetControl.sh
@@ -20,22 +20,27 @@ resourceGroupName="rg-${clientName}"
 
 # Configurar Budget
 echo "Configurando o Budget..."
-az consumption budget create --budget-name "monthly-budget" --resource-group $resourceGroupName --amount 20 --time-grain Monthly --start-date $(date +%Y-%m-01) --end-date $(date -d "+1 year" +%Y-%m-01) --category Cost --notifications '{
-  "email": {
-    "enabled": true,
-    "operator": "GreaterThan",
-    "threshold": 90,
-    "contactEmails": ["Joel.fernandes@datavizio.com.br"]
-  }
-}' --category Cost || abort_on_failure "Budget Mensal"
 
-az consumption budget create --budget-name "daily-budget" --resource-group $resourceGroupName --amount 10 --time-grain Daily --start-date $(date +%Y-%m-01) --end-date $(date -d "+1 year" +%Y-%m-01) --category Cost --notifications '{
-  "email": {
-    "enabled": true,
-    "operator": "GreaterThan",
-    "threshold": 90,
-    "contactEmails": ["Joel.fernandes@datavizio.com.br"]
-  }
-}' --category Cost || abort_on_failure "Budget Diário"
+# Criar o orçamento mensal
+az consumption budget create --budget-name "monthly-budget" --resource-group $resourceGroupName --amount 20 --time-grain Monthly --start-date $(date +%Y-%m-01) --end-date $(date -d "+1 year" +%Y-%m-01) --category Cost || abort_on_failure "Budget Mensal"
 
-echo "Configuração de notificações e budget concluída."
+# Criar o orçamento diário
+az consumption budget create --budget-name "daily-budget" --resource-group $resourceGroupName --amount 10 --time-grain Daily --start-date $(date +%Y-%m-01) --end-date $(date -d "+1 year" +%Y-%m-01) --category Cost || abort_on_failure "Budget Diário"
+
+echo "Configuração de Budget concluída."
+
+# Configurar notificações por e-mail para o orçamento mensal
+echo "Configurando notificações por e-mail para o Budget Mensal..."
+az monitor action-group create --name "${clientName}-monthly-budget-action" --resource-group $resourceGroupName --action email --emails "Joel.fernandes@datavizio.com.br" || abort_on_failure "Configuração de Ação para Budget Mensal"
+
+# Vincular a ação ao alerta de gastos mensais
+az monitor alert create --name "${clientName}-monthly-budget-alert" --resource-group $resourceGroupName --condition "spending > 90" --target "subscription/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Consumption/budgets/monthly-budget" --action "${clientName}-monthly-budget-action" --description "Notificação de orçamento mensal para o cliente ${clientName}" --frequency "Hour" --time-grain "1" --timezone "UTC" --start-date "$(date -u '+%Y-%m-%dT11:00:00')" --end-date "$(date -u '+%Y-%m-%dT18:00:00')" || abort_on_failure "Notificação de E-mail para Budget Mensal"
+
+# Configurar notificações por e-mail para o orçamento diário
+echo "Configurando notificações por e-mail para o Budget Diário..."
+az monitor action-group create --name "${clientName}-daily-budget-action" --resource-group $resourceGroupName --action email --emails "Joel.fernandes@datavizio.com.br" || abort_on_failure "Configuração de Ação para Budget Diário"
+
+# Vincular a ação ao alerta de gastos diários
+az monitor alert create --name "${clientName}-daily-budget-alert" --resource-group $resourceGroupName --condition "spending > 90" --target "subscription/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Consumption/budgets/daily-budget" --action "${clientName}-daily-budget-action" --description "Notificação de orçamento diário para o cliente ${clientName}" --frequency "Hour" --time-grain "1" --timezone "UTC" --start-date "$(date -u '+%Y-%m-%dT11:00:00')" --end-date "$(date -u '+%Y-%m-%dT18:00:00')" || abort_on_failure "Notificação de E-mail para Budget Diário"
+
+echo "Configuração de notificações por e-mail concluída."
